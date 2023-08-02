111832059a200453351e5572fbc11ab97ecb9f42
monica88lima <flima.monica@gmail.com>
2023-08-01 21:20:41 -0300

    Inclusão dos métodos HTTP

--- Tarefa_MinimalAPI/ApiTarefas/ApiTarefas/Program.cs
+++ Tarefa_MinimalAPI/ApiTarefas/ApiTarefas/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,11 +21,46 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapGet("/tarefas", async (AppDbContext db) =>  await db.Tarefas.ToListAsync());
+
+app.MapGet("/tarefa/ID{id}", async (int id, AppDbContext db) => 
+                          await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound());
+
+
+app.MapGet("/tarefa/concluida", async (AppDbContext db) =>
+                          await db.Tarefas.Where(x => x.IsCompleted).ToListAsync());
+
+app.MapGet("/tarefa/Nome{título}", async (string nome, AppDbContext db) =>
+                          await db.Tarefas.Where(x => x.Nome.Contains(nome)).ToListAsync());
+
 app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
 {
     db.Tarefas.Add(tarefa);
     await db.SaveChangesAsync();
     return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
+});
+
+app.MapPut("/tarefas/{id}", async (int id, Tarefa inputTarefa, AppDbContext db) =>
+{
+    var tarefa = await db.Tarefas.FindAsync(id);
+    if(tarefa is null) return Results.NotFound();
+
+    tarefa.Nome = inputTarefa.Nome;
+    tarefa.IsCompleted = inputTarefa.IsCompleted;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+
+});
+
+app.MapDelete("/tarefas/{id}", async (int id, AppDbContext db) =>
+{
+    if (await db.Tarefas.FindAsync(id) is Tarefa tarefa)
+    {
+        db.Tarefas.Remove(tarefa);
+        await db.SaveChangesAsync();
+        return Results.Ok(tarefa);
+    }
+    return Results.NotFound();
 });
 
 
---
+++ Tarefa_MinimalAPI/Readme.md
@@ -0,0 +1,9 @@
+# Minimal Api
+
+**Simplicidade na construção da Api, elas utilizam menos recursos, sendo uma boa para implemntação de microserviços, pois possuem  o minimo de dependencia do ASP**
+
+As rotas são configuradas na classe Program.cs
+
+:point_right:Template API WEB do ASP .NET CORE/ Tirar a flag dos controladores  
+
+:notebook:Documentação Disponivel https://learn.microsoft.com/pt-br/aspnet/core/tutorials/min-web-api?view=aspnetcore-7.0&tabs=visual-studio
